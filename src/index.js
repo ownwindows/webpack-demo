09c3e6baf1aed5010632264146da8125472fcbf0
Marinus Eigenraam <marinus.eigenraam@finalist.nl>
2022-06-27 18:25:41 +0200

    output management

--- src/index.js
+++ src/index.js
@@ -1,37 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png'
-import Data from './data.xml'
-import Notes from './data.csv'
-import toml from './data.toml'
-import yaml from './data.yaml';
-import json from './data.json5'
-
-
-console.log(toml.title)
-console.log(toml.owner.name)
-
-console.log(yaml.title)
-console.log(yaml.owner.name)
-
-console.log(json.title)
-console.log(json.owner.name)
+import printMe from './print'
 
 function component() {
     const element = document.createElement('div')
+    const btn = document.createElement( 'button' );
 
     // Lodash now imported by this script
     element.innerHTML = _.join( [ 'Hello', 'webpack' ], '' );
-    element.classList.add( 'hello' );
 
-    // add image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    element.appendChild( myIcon );
-
-    console.log(Data)
-    console.log(Notes)
+    element.appendChild(btn)
 
     return element;
 }
