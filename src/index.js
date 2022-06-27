2c0012ac6224485d679ba076db2322c008c6e9f0
Marinus Eigenraam <marinus.eigenraam@finalist.nl>
2022-06-27 17:50:38 +0200

    asset management

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png'
+import Data from './data.xml'
+import Notes from './data.csv'
+import toml from './data.toml'
+import yaml from './data.yaml';
+import json from './data.json5'
+
+
+console.log(toml.title)
+console.log(toml.owner.name)
+
+console.log(yaml.title)
+console.log(yaml.owner.name)
+
+console.log(json.title)
+console.log(json.owner.name)
 
 function component() {
     const element = document.createElement('div')
@@ -14,6 +29,9 @@ function component() {
     myIcon.src = Icon;
 
     element.appendChild( myIcon );
+
+    console.log(Data)
+    console.log(Notes)
 
     return element;
 }
